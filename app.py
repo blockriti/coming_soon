55c9ce5f963c50682085ba0cde87200e88f3957f
devesh aggrawal <deveshaggrawal19@gmail.com>
2017-04-06 00:54:58 +0530

    First commint soon template

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database import Subscribe, Base
+
+
+engine = create_engine('mysql+pymysql://root:waaslemysqlroot@127.0.0.1:3306/blockriti')
+Base.metadata.bind = engine
+Data_session = sessionmaker(bind=engine)
+session = Data_session()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def coming_soon():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        new = Subscribe()
+        new.email = request.form.get('email')
+        try:
+            session.add(new)
+            session.commit()
+        except:
+            session.rollback()
+    return redirect(url_for('coming_soon'))
+
+if __name__ == "__main__":
+    app.run()
---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Subscribe(Base):
+    __tablename__ = 'restaurant'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(80), nullable=False, unique=True)
+
+
+engine = create_engine('mysql+pymysql://root:waaslemysqlroot@127.0.0.1:3306/blockriti')
+Base.metadata.create_all(engine)
